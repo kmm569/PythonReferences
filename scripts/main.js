3935a84e9c89c9b4b1a419fff5f250ab8aed9696
Kaitlyn Musial <43589019+GothamGamer117@users.noreply.github.com>
2023-05-23 16:15:49 -0400

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+let pageIds = ['functions', 'lists', 'forLoops', 'lambda', 'maps'];
+let pageNames = ['Functions', 'Lists', 'For Loops', 'Lambda', 'Maps'];
+
+window.onload = () => {
+    let linkList = document.getElementById('linkList');
+
+    let links = [];
+    for (var item in pageNames) {
+        let div = document.createElement('div');
+        let listLink = document.createElement('li');
+        let newLink = document.createElement('a');
+        listLink.classList.add('link');
+        listLink.id = pageIds[item];
+        newLink.innerHTML = `${pageNames[item]}`
+        listLink.append(newLink);
+        div.append(listLink);
+        links.push(div.innerHTML);
+    }
+
+    linkList.innerHTML = links.join(' | ');
+
+    let pageLinks = document.getElementsByClassName('link');
+
+    for (let l of pageLinks) {
+        l.addEventListener('click', () => {
+            for (var x in pageIds) {
+                document.getElementById(pageIds[x] + '-div').classList.remove('show');
+            }
+            document.getElementById(`${l.id}-div`).classList.add('show');
+        })
+    }
+}
